773346fd3383d668d1c0b1c83176a4accb9c53f3
khusshii26 <khushippal@gmail.com>
2026-05-07 11:41:17 +0530

    Time: 371 ms (33.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,30 @@
+WITH daily AS
+(
+    SELECT visited_on,
+           SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+),
+
+window_sum AS
+(
+    SELECT visited_on,
+           SUM(amount) OVER
+           (
+               ORDER BY visited_on
+               ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+           ) AS total_amount,
+           
+           COUNT(*) OVER
+           (
+               ORDER BY visited_on
+               ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+           ) AS cnt
+    FROM daily
+)
+
+SELECT visited_on,
+       total_amount AS amount,
+       ROUND(total_amount / 7, 2) AS average_amount
+FROM window_sum
+WHERE cnt = 7;
