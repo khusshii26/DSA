faa06f35266ee732ca8f25fd8f22e3a20f28d30d
khusshii26 <khushippal@gmail.com>
2026-06-11 21:09:00 +0530

    Time: 83 ms (80.95%) | Memory: 281.8 MB (54.76%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.java
@@ -0,0 +1,57 @@
+class Solution {
+    static final long MOD = 1_000_000_007L;
+
+    public int assignEdgeWeights(int[][] edges) {
+        int n = edges.length + 1;
+
+        List<Integer>[] graph = new ArrayList[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[n + 1];
+
+        q.offer(1);
+        vis[1] = true;
+
+        int depth = -1;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            depth++;
+
+            while (size-- > 0) {
+                int u = q.poll();
+
+                for (int v : graph[u]) {
+                    if (!vis[v]) {
+                        vis[v] = true;
+                        q.offer(v);
+                    }
+                }
+            }
+        }
+
+        return (int) modPow(2, depth - 1);
+    }
+
+    private long modPow(long a, long b) {
+        long res = 1;
+
+        while (b > 0) {
+            if ((b & 1) == 1)
+                res = (res * a) % MOD;
+
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return res;
+    }
+}
